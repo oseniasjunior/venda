5fa921c0640a2725d2fe1510fc0c29a6cad28c95
Unknown <oseniasjunior@gmail.com>
2019-05-17 19:19:25 -0400

    Add new queries

--- core/queries.py
+++ core/queries.py
@@ -1,78 +1,82 @@
-from django.db.models import Q, Value, CharField, ExpressionWrapper, F, DecimalField
-from django.db.models.functions import Upper, Lower, LPad, Cast
+from itertools import chain
 
-from core import models as core_models
+from django.db.models import OuterRef, Subquery, Sum, FloatField, Case, When, Q, CharField, Count, Value, \
+    ExpressionWrapper, F, Avg
 
-
-def query1():
-    q1 = core_models.Product.objects.all()
-    q2 = core_models.Product.objects.filter(name__startswith='a')
-    q3 = core_models.Product.objects.order_by('-id')
-    q4 = core_models.Product.objects.values('id', 'name')
-    q5 = core_models.Product.objects.get(pk=5)
-    q6 = core_models.Product.objects.order_by('-id').first()
-    q7 = core_models.Product.objects.order_by('id').last()
-    q8 = core_models.Product.objects.exists()
-    q9 = core_models.Product.objects.count()
-    q10 = core_models.Product.objects.filter(Q(id__gt=4) | Q(name__startswith='a'))
-    return q1, q2, q3, q4, q5, q6, q7, q8, q9
+from core import models
 
 
-def query2():
-    return core_models.Product.objects.values('name', 'product_group__description')
-
-
-def query3():
-    query = core_models.ProductGroup.objects.filter(description__in=['Limpeza', 'Bebidas'])
-    return core_models.Product.objects.filter(product_group__in=query)
-
-
-def query4():
-    return core_models.Product.objects.filter(product_group__description__in=['Limpeza', 'Bebidas'])
-
-
-def query5():
-    return core_models.Product.objects.annotate(
-        code=LPad(
-            Cast('id', output_field=CharField()),
-            6,
-            Value('0')
-        ),
-        name_upper=Upper('name'),
-        name_lower=Lower('name')
-    ).values('code', 'name_upper', 'name_lower')
-
-
-def query6():
-    return core_models.Product.objects.annotate(
-        custom_code=LPad(
-            Cast('id', output_field=CharField()),
-            6,
-            Value('0')
-        ),
-    ).annotate(
-        gain_price=ExpressionWrapper(
-            F('sale_price') - F('cost_price'),
-            output_field=DecimalField()
-        )
-    ).values('custom_code', 'name', 'gain_price')
-
-
-def query7():
-    return core_models.SaleItem.objects.annotate(
-        custom_code=LPad(
-            Cast('id', output_field=CharField()),
-            6,
-            Value('0')
-        ),
-        subtotal=ExpressionWrapper(
-            F('quantity') * F('product__sale_price'),
-            output_field=DecimalField()
-        )
-    ).order_by('product__name').values(
-        'custom_code',
-        'product__name',
-        'quantity',
-        'product__sale_price',
-        'subtotal'
+def query9():
+    subquery = models.Employee.objects.annotate(
+        sum_salary=Sum('salary')
+    ).filter(
+        departament=OuterRef('pk')
     )
+    subquery.query.group_by = None
+    queryset = models.Departament.objects.annotate(
+        total=Subquery(subquery.values('sum_salary'), output_field=FloatField())
+    )
+    return queryset.values('name', 'total')
+
+
+def query10():
+    queryset = models.Customer.objects.annotate(
+        _gender=Case(
+            When(condition=Q(gender=models.Customer.FEMALE), then=Value('FEMININO')),
+            default=Value('MASCULINO'),
+            output_field=CharField()
+        )
+    ).values('_gender').annotate(
+        count_gender=Count('*')
+    ).values('_gender', 'count_gender')
+    return queryset
+
+
+def query11(year: int = 2011):
+    subquery = models.SaleItem.objects.select_related('product', 'sale').annotate(
+        subtotal=Sum(ExpressionWrapper(
+            F('quantity') * F('product__sale_price'), output_field=FloatField()
+        ), output_field=FloatField())
+    ).filter(
+        product__product_group=OuterRef('id'),
+        sale__date__year=year
+    )
+    subquery.query.group_by = None
+    queryset = models.ProductGroup.objects.annotate(
+        sale_total=Subquery(subquery.values('subtotal'), output_field=FloatField())
+    ).values(
+        'description', 'sale_total'
+    )
+    return queryset
+
+
+def query12():
+    models.SaleItem.objects.select_related('product', 'product__product_group').values(
+        'product__product_group__description'
+    ).annotate(
+        sale_total=Sum(ExpressionWrapper(
+            F('quantity') * F('product__sale_price'), output_field=FloatField()
+        ), output_field=FloatField())
+    ).values('product__product_group__description', 'sale_total')
+
+
+def gain_by_product_group(year: int):
+    return models.SaleItem.objects.select_related('sale', 'product', 'product__product_group').annotate(
+        subtotal=ExpressionWrapper(F('quantity') * F('product__sale_price'), output_field=FloatField())
+    ).filter(sale__date__year=year).values('product__product_group__description').annotate(
+        gain=Sum(ExpressionWrapper(
+            F('subtotal') * F('product__product_group__gain_percentage') / Value(100), output_field=FloatField()
+        ), output_field=FloatField())
+    ).values('product__product_group__description', 'gain')
+
+
+def total_customer_by_marital_status():
+    return models.Customer.objects.select_related('marital_status').values(
+        'marital_status__description'
+    ).annotate(counter=Count('id')).values('marital_status__description', 'counter')
+
+
+def total_customer_by_zone():
+    return models.Customer.objects.select_related('district', 'district__zone').values(
+        'district__zone__name'
+    ).annotate(counter=Count('id')).values('district__zone__name', 'counter')
