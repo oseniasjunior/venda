00b9aeeeccd1a8fa8263eec9e0848a4b9c0b8a56
Unknown <oseniasjunior@gmail.com>
2019-05-22 18:25:18 -0400

    Add admins

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from core import models
+
+
+@admin.register(models.StockAddress)
+class StockAddressModelAdmin(admin.ModelAdmin):
+    pass
+
+
+class SaleItemInLine(admin.TabularInline):
+    model = models.SaleItem
+    readonly_fields = ['subtotal']
+    extra = 1
+
+
+@admin.register(models.Sale)
+class SaleModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'employee', 'customer')
+    list_per_page = 10
+    inlines = [SaleItemInLine]
