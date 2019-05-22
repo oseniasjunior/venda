5561819c5cf9b1e28cff0c2d910710aa51b34944
Unknown <oseniasjunior@gmail.com>
2019-05-22 19:20:59 -0400

    Add new filters

--- core/filters.py
+++ core/filters.py
@@ -1,6 +1,12 @@
+from django.db.models import QuerySet, Q
 from django_filters import filterset
+from django_filters.widgets import BooleanWidget
 
 from core import models, choices
+
+
+class CharInFilter(filterset.BaseInFilter, filterset.CharFilter):
+    pass
 
 
 class ZoneFilterSet(filterset.FilterSet):
@@ -15,6 +21,14 @@ class DistrictFilterSet(filterset.FilterSet):
     city = filterset.CharFilter(
         lookup_expr=choices.LIKE,
         field_name='city__name'
+    )
+    start = filterset.DateFilter(
+        lookup_expr='date__gte',
+        field_name='created_at'
+    )
+    end = filterset.DateFilter(
+        lookup_expr='date__lte',
+        field_name='created_at'
     )
 
     class Meta:
@@ -31,3 +45,35 @@ class SaleFilterSet(filterset.FilterSet):
     class Meta:
         model = models.Sale
         fields = ['year']
+
+
+class MovementStockFilterSet(filterset.FilterSet):
+    type = CharInFilter(lookup_expr=choices.IN)
+    active = filterset.BooleanFilter(widget=BooleanWidget)
+    start = filterset.DateFilter(
+        lookup_expr='gte',
+        field_name='date'
+    )
+    end = filterset.DateFilter(
+        lookup_expr='lte',
+        field_name='date'
+    )
+
+    class Meta:
+        model = models.MovementStock
+        fields = ['type', 'active', 'start', 'end']
+
+
+class ProductFilterSet(filterset.FilterSet):
+    product_or_group = filterset.CharFilter(
+        method='filter_product_or_group'
+    )
+
+    def filter_product_or_group(self, queryset: QuerySet, name, value):
+        return queryset.filter(
+            Q(name__icontains=value) | Q(product_group__description__icontains=value)
+        )
+
+    class Meta:
+        model = models.Product
+        fields = ['product_or_group']
