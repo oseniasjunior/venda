8c60641666262834a8bcb50cd601d6c84f4e3d25
Unknown <oseniasjunior@gmail.com>
2019-05-22 20:18:12 -0400

    Add celery.py

---
+++ venda/celery.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+# set the default Django settings module for the 'celery' program.
+from kombu import Exchange, Queue
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'venda.settings')
+
+app = Celery()
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+
+# app.conf.task_queues = (
+#     Queue('alguma', Exchange('alguma'), routing_key='alguma'),
+# )
