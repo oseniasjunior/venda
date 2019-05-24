25e5b1bcfcd4eea85fbf9a4d19231812e868c22e
Unknown <oseniasjunior@gmail.com>
2019-05-23 21:10:06 -0400

    Add consumer

---
+++ core/consumers.py
@@ -0,0 +1,32 @@
+from urllib import parse
+
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
+
+
+class ConsumerBase(AsyncJsonWebsocketConsumer):
+
+    def get_query_params(self):
+        query_string = self.scope['query_string'].decode()
+        return dict(parse.parse_qsl(query_string))
+
+    def get_group_name(self):
+        raise NotImplementedError('You must create subclasses and implement this method')
+
+    async def connect(self):
+        await self.accept()
+        await self.channel_layer.group_add(self.get_group_name(), self.channel_name)
+
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(self.get_group_name(), self.channel_name)
+
+    async def group_message(self, event):
+        await self.send_json(content=event['content'])
+
+    async def receive_json(self, event, **kwargs):
+        await self.send_json(content=event['content'])
+
+
+class ProductConsumer(ConsumerBase):
+    def get_group_name(self):
+        query_params = self.get_query_params()
+        return 'product-{}'.format(query_params.get('product'))
