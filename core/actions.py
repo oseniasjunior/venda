c0cd03245db081ea768ee88c3f59a57ad0ab3155
Unknown <oseniasjunior@gmail.com>
2019-05-23 21:37:12 -0400

    Send channels message

--- core/actions.py
+++ core/actions.py
@@ -1,3 +1,5 @@
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
 from django.utils.timezone import now
 
 from core import models
@@ -25,3 +27,11 @@ def generate_movement_stock_item(instance: models.SaleItem):
         'quantity': instance.quantity
     }
     models.MovementStockItem.objects.create(**payload)
+
+
+def send_channel_message(group_name: str, content: dict) -> None:
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(group_name, {
+        "type": "group.message",
+        "content": content
+    })
